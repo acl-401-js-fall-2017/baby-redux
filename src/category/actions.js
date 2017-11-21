f8a2b15278890c41484989960ce0fedb54ba1c23
Zac-L <Zachlichens@gmail.com>
2017-11-20 23:05:15 -0800

    created add update and remove functions

---
+++ src/category/actions.js
@@ -0,0 +1,26 @@
+import * as actions from './constants';
+import shortid from 'shortid';
+
+export function addCategory(category) {
+
+  category._id = shortid.generate();
+  category.timestamp = new Date();
+  return {
+    type: actions.CATEGORY_ADD,
+    payload: category
+  };
+}
+
+export function updateCategory(category) {
+  return {
+    type: actions.CATEGORY_UPDATE,
+    payload: category
+  };
+}
+
+export function removeCategory(id) {
+  return {
+    type: actions.CATEGORY_REMOVE,
+    payload: id
+  };
+}
