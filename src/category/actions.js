7367cca9586a1387a62c7c24ee2f65ae005994b2
Robin Miller <rmillerpdx@gmail.com>
2017-11-21 11:14:29 -0800

    create store.js

--- src/category/actions.js
+++ src/category/actions.js
@@ -4,13 +4,12 @@ import shortid from 'shortid';
 export function addCategory(category)  {
   category._id = shortid.generate();
   category.timestamp = new Date();
-  
+
   return {
     type: actions.CATEGORY_ADD,
     payload: category
   };
 }
-
 
 export function updateCategory(category) {
   return {
@@ -18,7 +17,6 @@ export function updateCategory(category) {
     payload: category
   };
 }
-
 
 export function removeCategory(id) {
   return {
