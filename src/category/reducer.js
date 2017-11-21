21208448b5f5bf75a21fe49384af7b3611902c84
ckrountree <jmail0207@gmail.com>
2017-11-21 10:29:40 -0800

    add update and remove cases in reducer

--- src/category/reducer.js
+++ src/category/reducer.js
@@ -1,12 +1,18 @@
 import * as actions from './constants';
 
-export default function category(state = [], { type, payload }) {  //destructured "action"
+export default function categoryReducer(state = [], { type, payload }) {  //destructured "action"
   switch(type) {
     case actions.CATEGORY_ADD:
       return  [
         ...state,
         payload
       ];
+
+    case actions.CATEGORY_UPDATE:
+      return state.map(category => category._id === payload._id ? { ...category, ...payload } : category);
+
+    case actions.CATEGORY_REMOVE:
+      return state.filter(category => category._id !== payload._id);
 
     default: 
       return state;
