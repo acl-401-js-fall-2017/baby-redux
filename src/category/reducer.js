0881b5145468b33fefc2486bd49362086556abc0
Zac-L <Zachlichens@gmail.com>
2017-11-20 22:57:59 -0800

    created a reducer file to hold all interactions

---
+++ src/category/reducer.js
@@ -0,0 +1,22 @@
+import * as actions from './constants';
+
+export default function category(state = [], { type, payload }) {
+
+  switch(type) {
+
+    case actions.CATEGORY_ADD:
+      return [
+        ...state,
+        payload
+      ];
+
+    case actions.CATEGORY_REMOVE:
+      return state.filter(c => c._id !== payload);
+
+    case actions.CATEGORY_UPDATE:
+      return state.map(c => c._id === payload._id ? { ...c, ...payload } : c);
+
+    default:
+      return state;
+  }
+}
