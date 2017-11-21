49c42ccdb3c7b7b1ea33b213bd1e5bfdf8469a50
ckrountree <jmail0207@gmail.com>
2017-11-21 13:18:17 -0800

    add category functionality

--- src/App.js
+++ src/App.js
@@ -14,9 +14,7 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to My Budget App</h1>
           </header>
-          <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Categories/>
         </div>
       </Router>
     );
---
+++ src/category/CategoryForm.js
@@ -0,0 +1,50 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+export default class CategoryForm extends PureComponent {
+
+  static propTypes = {
+    category: PropTypes.object,
+    onComplete: PropTypes.func.isRequired
+  }
+
+  static defaultProps = {
+    text: 'Add'
+  }
+
+  constructor(props) {
+    super(props);
+    const { category = {} } = props;
+    this.state = {
+      id: category._id || null,
+      timestamp: category.timestamp,
+      name: category.name || null,
+      budget: category.budget
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { _id, timestamp, name, budget } = this.state;
+    this.props.onComplete({ _id, timestamp, name, budget });
+  }   
+
+  handleChange = ({ target: input }) => {
+    this.setState({
+      [input.name]: input.value
+    });
+  }
+  
+
+  render() {
+    const { name } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+        Name: <input name="name" value={name} onChange={this.handleChange}/>
+        </div>
+        <button type="submit">{this.props.text}</button>
+      </form>
+    );
+  }
+}
