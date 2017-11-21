441edc31d03bf572e75365138e3cc5ef4734a8e4
Zac-L <Zachlichens@gmail.com>
2017-11-21 11:48:37 -0800

    added categories to app

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+import Categories from './category/Categories';
+
+
 class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Categories/>
+        </div>
       </Router>
     );
   }
