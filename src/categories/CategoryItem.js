0ab8113456a98c252ce5ad0d3bf7826f21d3605e
Shane Moyo <smoyo@pdx.edu>
2017-11-28 07:16:31 -0800

    fixing file names as per readme.md

---
+++ src/categories/CategoryItem.js
@@ -0,0 +1,48 @@
+import { removeCategory, updateCategory } from '../categories/actions';
+import React, { PureComponent } from 'react';
+import CategoryForm from './CategoryForm';
+import { connect } from 'react-redux';
+
+class CategoryItem extends PureComponent {
+  
+  handleUpdate = category => event => {
+    event.preventDefault();
+    const update = {
+      ...category,
+      name: event.target.name.value,
+      budget: event.target.budget.value
+    }
+    this.props.onUpdateCategory(update);
+  }
+  
+  handleRemove = id => () => this.props.onRemoveCategory({ id });
+  render() {
+    const { category } = this.props;
+    return (
+      <div>
+        <h1>{category.name}</h1>
+        <p>Budget - {category.budget}</p>
+        <CategoryForm onComplete={this.handleUpdate(category)} buttonText={'edit'}/>
+        <input type="button" value="remove" onClick={this.handleRemove(category.id)}/>
+      </div>
+    );
+  }
+}
+
+
+
+
+const match = dispatch => {
+  return {
+    onRemoveCategory: id => {
+      dispatch(removeCategory(id));
+    },
+    onUpdateCategory: category => {
+      dispatch(updateCategory(category));
+    }
+  };
+}
+
+const connectedCategoryItem = connect(null, match)(CategoryItem);
+
+export default connectedCategoryItem;
