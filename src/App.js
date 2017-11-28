0515bd0cc07ff0de4f542f62863df8eb5e56a97f
Shane Moyo <smoyo@pdx.edu>
2017-11-28 07:10:03 -0800

    restructuring app.js  removing store file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
-import Categories from './category/Categories';
-import logo from './logo.svg';
+import { BrowserRouter as Router,
+   Route,
+    Switch,
+     Redirect
+    } from 'react-router-dom';
+import Dashboard from './dashboard/Dashboard';
+
 import './App.css';
-import AddCategory from './category/AddCategory';
 
 class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <NavLink exact to="/categories">Categories</NavLink>
-          <span></span>
-          <NavLink to ="/categories/addcategory">Add Category</NavLink>
-        </header>
-        <Switch>
-          <Route path="/categories/addcatgeory" component={AddCategory}/>
-          <Route path="/categories" component={Categories}/>
-        </Switch>
-      </div>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Dashboard}/>>
+            <Redirect to="/"/>
+          </Switch>
+        </div>
       </Router>
     );
   }
