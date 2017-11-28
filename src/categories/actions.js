51c35f5df718522002fee1ee8e4cf95439c3e8eb
Shane Moyo <smoyo@pdx.edu>
2017-11-28 07:19:32 -0800

    fixed actions

--- src/categories/actions.js
+++ src/categories/actions.js
@@ -1,26 +1,23 @@
-import * as actions from './constants';
 import shortid from 'shortid';
+import * as actions from './constants';
 
-export function addCategory(category){
-    category._id = shortid.generate();
-    category.timestamp = new Date();
-    return{
-        type: actions.CATEGORY_ADD,
-        payload: category
-    };
+export const addCategory = ({ name, budget }) => {
+  return {
+    type: actions.CATEGORY_ADD,
+    payload: {
+      id: shortid.generate(),
+      timestamp: new Date(),
+      name,
+      budget
+    }
+  };
 }
 
-export function updateCategory(update){
-    return {
-        type: actions.CATEGORY_UPDATE,
-        payload: update
-    };
+export const removeCategory = ({ id }) => {
+  return { type: actions.CATEGORY_REMOVE, payload: { id }};
 }
 
-export function removeCategory(id){
-    return {
-        type: actions.CATEGORY_REMOVE,
-        payload: id
-    };
+export const updateCategory = (category) => {
+  return { type: actions.CATEGORY_UPDATE, payload: category
+  };
 }
-
