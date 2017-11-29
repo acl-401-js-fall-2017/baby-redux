911a08047dc431d0a8c3742acd77c11c4696f8ab
mirrorapps <eli.speigel@gmail.com>
2017-11-29 10:12:34 -0800

    added category reducer

---
+++ src/category/reducer.js
@@ -0,0 +1,19 @@
+import * as actions from './constants';
+
+export default function categories(state = [], { type, payload }) {
+  switch (type) {
+    case actions.CATEGORY_ADD:
+      return [
+        ...state,
+        payload
+      ];
+    
+    case actions.CATEGORY_REMOVE:
+      return state.filter(c => c._id !== payload);
+    
+    case actions.CATEGORY_UPDATE:
+      return state.map(c => c._id === payload._id ? { ...c, ...payload } : c);
+
+    default: return state;
+  }
+}
