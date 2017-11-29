2a1a170212714d8edc81ad77749cd432a718c211
mirrorapps <eli.speigel@gmail.com>
2017-11-29 11:05:59 -0800

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Category from './category/Category';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Category />
-      </div>
+      <Router>
+        <div className="App">
+          <Category />
+        </div>
+      </Router>
     );
   }
 }
