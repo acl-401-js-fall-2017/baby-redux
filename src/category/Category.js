dba3ea8785a1192426f6f909f73f58870224a250
mirrorapps <eli.speigel@gmail.com>
2017-11-29 10:12:06 -0800

    added render

--- src/category/Category.js
+++ src/category/Category.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import shortid from 'shortid';
 
-
 class Category extends Component {
-    constructor() {
-        super();
-        this.state = {
-            _id: shortid.generate(),
-            timestamp: new Date(),
-            name: '',
-            budget: null
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      _id: shortid.generate(),
+      timestamp: new Date(),
+      name: '',
+      budget: null
+    };
+  }
+  render() {
+    return(
+      <p>filler</p>
+    );
+  }
 }
+
+export default Category;
