bc76a2ae5708d8e1b397d238a90b7bfb0d644d5f
David Nishikawa <triptype@gmail.com>
2017-11-29 10:28:16 -0800

    write api routes

---
+++ src/services/api.js
@@ -0,0 +1,35 @@
+const url = '/api';
+
+const wrap = promise => {
+  return promise.then(response => response.json());
+};
+
+export const get = path => wrap(fetch(`${url}${path}`));
+
+export const post = (path, data) => wrap(
+  fetch(`${url}${path}`, {
+    method: 'post',
+    body: JSON.stringify(data),
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+);
+
+export const patch = (path, data) => wrap(
+  fetch(`${url}${path}`, {
+    method: 'PATCH',
+    body: JSON.stringify(data),
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+);
+
+export const remove = path => wrap(
+  fetch(`${url}${path}`, {
+    method: 'DELETE'
+  })
+);
