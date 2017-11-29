d563e310310ef028b91751660f365bc85ded4740
Shane Moyo <smoyo@pdx.edu>
2017-11-29 05:40:31 -0800

    fixing travis

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import { Provider } from 'react-redux';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import categories from './categories/reducer';
+import { Provider } from 'react-redux';
+import categories from './categories/reducers';
 import './index.css';
 import App from './App';
-import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(categories);
+const store = createStore(
+  categories,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render((
   <Provider store={store}>
