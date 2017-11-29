8821963e10da5540b9f0519aca8d6362acddd860
Zac-L <Zachlichens@gmail.com>
2017-11-28 17:18:31 -0800

    removed unneeded console.log

--- src/category/reducer.test.js
+++ src/category/reducer.test.js
@@ -11,7 +11,6 @@ describe('category reducers', () => {
   it('adds a category', () => {
     const category = {  name: 'Zach', budget: 1000 };
     const state = reducer([], { type: actions.CATEGORY_ADD, payload: category });
-    console.log();
     expect(state).toEqual([category]);
   });
 
