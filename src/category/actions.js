184e38b208f87c9f0b1b3c842a9794caf1d8606a
Zac-L <Zachlichens@gmail.com>
2017-11-29 13:08:29 -0800

    updated add function with dispatch and async, also created loadall function

--- src/category/actions.js
+++ src/category/actions.js
@@ -1,11 +1,24 @@
 import * as actions from './constants';
 import categoryApi from '../services/categories.api';
 
-export function addCategory(category) {
+export function loadCategories() {
 
   return async dispatch => {
 
     const categories = await categoryApi.get();
+
+    dispatch({
+      type: actions.CATEGORY_LOAD,
+      payload: categories
+    });
+  };
+}
+
+export function addCategory(category) {
+
+  return async dispatch => {
+
+    const categories = await categoryApi.add(category);
     dispatch({
       type: actions.CATEGORY_ADD,
       payload: categories
