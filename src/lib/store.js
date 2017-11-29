7752033465a735e2ebcc8eb03186db6a93a33ea5
Zac-L <Zachlichens@gmail.com>
2017-11-29 09:30:57 -0800

    working on getting rootReducer to work

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,15 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
-// import rootReducer from './rootReducer';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// const store = createStore(
-//   rootReducer,
-//   composeEnhancers(applyMiddleware(thunk))
-// );
-import category from '../category/reducer';
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+// import category from '../category/reducer';
 
-const store = createStore(category);
+// const store = createStore(category);
 
 export default store;
