192338e7922bbeb076e843f06551f935fee7d64b
ckrountree <jmail0207@gmail.com>
2017-11-30 10:13:06 -0800

    update store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
+import { categoryReducer, categoryLoad, categoryError } from '../category/reducer';
 
-const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose;
+// const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const rootReducer = combineReducers({ categoryReducer, categoryLoad, categoryError });
 
 const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(thunk)
