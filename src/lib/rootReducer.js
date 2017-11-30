6bb0f85405cef555816c408604e89d9985bd666d
Zac-L <Zachlichens@gmail.com>
2017-11-30 12:17:03 -0800

    added load reducers to rootReducer

--- src/lib/rootReducer.js
+++ src/lib/rootReducer.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import  categories  from '../category/reducer';
+import { response, loading, error } from '../load/reducer';
 
-const rootReducer =  combineReducers({ categories });
+const rootReducer =  combineReducers({ 
+  categories, response, loading, error
+});
 
 export default rootReducer;
