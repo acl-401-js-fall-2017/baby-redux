6387134459333c59ad54170992f8127028859fe7
Zac-L <Zachlichens@gmail.com>
2017-11-30 12:17:25 -0800

    add LoadResponse to app to handle simulated loading and errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 
 import Categories from './category/Categories';
+import LoadResponse from './load/LoadResponse';
 
 
 class App extends Component {
   render() {
+    const { loading, error } = this.props;
+
     return (
       <Router>
         <div className="App">
@@ -17,18 +20,34 @@ class App extends Component {
             <h1 className="App-title">Welcome to React</h1>
           </header>
           <Categories/>
+          <footer>
+            <LoadResponse/>
+            {loading && 
+            <div>
+              Loading...
+            </div>
+            }
+            {error &&
+            <div>
+              {Array.isArray(error)
+                ? <ul>error.map(err => <li>err</li>)</ul>
+                : error.error ? error.error : error
+              }
+            </div>
+            }
+          </footer>
         </div>
       </Router>
     );
   }
 }
 
-// export default connect(
-//   state => ({
-//     loading: state.loading,
-//     error: state.error
-//   }),
-//   null
-// )(App);
+export default connect(
+  state => ({
+    loading: state.loading,
+    error: state.error
+  }),
+  null
+)(App);
 
-export default App;
+// export default App;
