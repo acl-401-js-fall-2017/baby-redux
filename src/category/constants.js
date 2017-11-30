a21955567bc27c2b3a7b9c19681f8c2bff58e72f
Zac-L <Zachlichens@gmail.com>
2017-11-30 09:50:16 -0800

    added constants for loading and error

--- src/category/constants.js
+++ src/category/constants.js
@@ -2,3 +2,6 @@ export const CATEGORY_ADD = 'CATEGORY_ADD';
 export const CATEGORY_UPDATE = 'CATEGORY_UPDATE';
 export const CATEGORY_REMOVE = 'CATEGORY_REMOVE';
 export const CATEGORY_LOAD = 'CATEGORY_LOAD';
+export const LOADING = 'LOADING';
+export const RESPONSE_LOAD = 'RESPONSE_LOAD';
+export const ERROR = 'ERROR';
