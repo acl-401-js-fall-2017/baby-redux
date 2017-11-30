c3b511ed63df58e9ed1e32ec6095505cf3f3311f
Shane Moyo <smoyo@pdx.edu>
2017-11-30 05:15:08 -0800

    added add category test

--- src/categories/reducer.test.js
+++ src/categories/reducer.test.js
@@ -1,10 +1,16 @@
 import reducer from './reducers';
-import * as actions from './actions';
+import * as actions from './constants';
 
 describe( 'Categories reducer', ()=> {
 
     it('Should intialize', () => {
         const state = reducer(undefined, {});
         expect(state).toEqual([]);
+    });
+
+    it('Adds a new budget category', () => {
+        const testBudget = { name: 'test budget', budget: 100 };
+        const state = reducer([], { type: actions.CATEGORY_ADD, payload: testBudget });
+        expect(state).toEqual([testBudget]);
     });
 })
