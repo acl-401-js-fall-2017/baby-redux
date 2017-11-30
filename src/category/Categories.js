3a1f4cb23f1453066afde609588823359b6ee7fd
Maryussuperboy <martsyalis@gmail.com>
2017-11-30 12:12:53 -0800

    delete useless code

--- src/category/Categories.js
+++ src/category/Categories.js
@@ -1,5 +1,4 @@
 import React, { PureComponent } from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addCategory, updateCategory, removeCategory, loadCategories } from './actions';
 
