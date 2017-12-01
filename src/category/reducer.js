f3141657746044b38f3a8906bc189d3fd83ac3d7
Andrew Bodey <a.s.bodey@gmail.com>
2017-12-01 10:53:01 -0800

    changed reducer store and constants

--- src/category/reducer.js
+++ src/category/reducer.js
@@ -1,6 +1,6 @@
 import * as actions from './constants';
 
-export default function moneys(state = [], { type, payload }) {
+export function budgets(state = [], { type, payload }) {
   switch (type) {
     case actions.CATEGORY_ADD:
       return [
@@ -13,5 +13,26 @@ export default function moneys(state = [], { type, payload }) {
       return state.map(p => p._id === payload._id ? { ...p, ...payload } : p);
     default:
       return state;    
+  }
+}
+
+export function budgetsLoading(state = false, { type, payload }) {
+  switch(type){
+    default:
+      return state;
+  }
+}
+
+export function budgetsError(state = null, { type, payload }) {
+  switch(type) {
+    case actions.CATEGORY_LOAD:
+    case actions.CATEGORY_ADD:
+    case actions.CATEGORY_REMOVE:
+    case actions.CATEGORY_UPDATE:
+      return null;
+    case actions.CATEGORY_ERROR:
+      return payload;
+    default:
+      return state;
   }
 }
--- src/store.js
+++ src/store.js
@@ -1,6 +1,19 @@
-import { createStore } from 'redux';
-import budgets from './category/reducer';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { budgets, budgetsLoading, budgetsError } from './category/reducer';
 
-const store = createStore(budgets);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  //list the functions from the reducer that is needs in this store
+  budgets,
+  budgetsLoading,
+  budgetsError
+});
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
