1a7204acbee4e35f560fcc46950fc028ee57c307
erdem koral <koral.erdem@gmail.com>
2017-12-01 11:41:09 -0800

    added some spinner css

--- src/Category/budget.js
+++ src/Category/budget.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { loadBudgets, addBudget, removeBudget, updateBudget } from './actions';
 import BudgetForm from './budgetForm';
+import '../App.css';
 
 class Budget extends Component {
 
