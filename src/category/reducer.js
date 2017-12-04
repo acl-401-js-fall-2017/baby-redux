704fcd30c81661b36851db5cc608d8c6df54827e
mirrorapps <eli.speigel@gmail.com>
2017-12-03 21:53:07 -0800

    syntax

--- src/category/reducer.js
+++ src/category/reducer.js
@@ -4,16 +4,16 @@ export const CATEGORY_REMOVE = 'CATEGORY_REMOVE';
 
 export default function categories(state = [], { type, payload }) {
   switch (type) {
-    case actions.CATEGORY_ADD:
+    case CATEGORY_ADD:
       return [
         ...state,
         payload
       ];
     
-    case actions.CATEGORY_REMOVE:
+    case CATEGORY_REMOVE:
       return state.filter(c => c._id !== payload);
     
-    case actions.CATEGORY_UPDATE:
+    case CATEGORY_UPDATE:
       return state.map(c => c._id === payload._id ? { ...c, ...payload } : c);
 
     default: return state;
