c2f0733af20c08a16a3e4e3d3b0a375a7e29c65a
Zac-L <Zachlichens@gmail.com>
2017-12-03 17:00:34 -0800

    built out a hopefully working expenses api call. Curently untested. In theory it should work

---
+++ src/services/expenses.api.js
@@ -0,0 +1,16 @@
+import api from './api';
+
+export default {
+  get(categoryId, expenseId) {
+    return api.get(`/categories/${categoryId}/expenses/${expenseId}`);
+  },
+  add(categoryId, expense) {
+    return api.post(`/categories/${categoryId}/expense`, expense);
+  },
+  remove(categoryId, expenseId) {
+    return api.delete(`/categories/${categoryId}/expenses/${expenseId}`);
+  },
+  update(categoryId, expenseId, data) {
+    return api.patch(`/categories/${categoryId}/expenses/${expenseId}`, data);
+  }
+};
