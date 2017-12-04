2c7a24fc0815a9e3009d37077d190f3d75f9b962
Zac-L <Zachlichens@gmail.com>
2017-12-04 12:17:51 -0800

    refactored category actions to account for new promisemiddleware

--- src/category/actions.js
+++ src/category/actions.js
@@ -1,72 +1,31 @@
 import * as actions from './constants';
 import categoryApi from '../services/categories.api';
 
+
 export function loadCategories() {
-
-  return async dispatch => {
-
-    const categories = await categoryApi.get();
-
-    dispatch({
-      type: actions.CATEGORY_LOAD,
-      payload: categories
-    });
+  return {
+    type: actions.CATEGORY_LOAD,
+    payload: categoryApi.get()
   };
 }
 
-export function addCategory(category) {
-
-  return async dispatch => {
-    const { name, budget } = category;
-    const categories = await categoryApi.add({ name, budget });
-    dispatch({
-      type: actions.CATEGORY_ADD,
-      payload: categories
-    });
+export function addCategory({ name, budget }) {
+  return {
+    type: actions.CATEGORY_ADD,
+    payload: categoryApi.add({ name, budget })
   };
 }
 
 export function updateCategory(category) {
-
-  return async dispatch => {
-    const updatedCategory = await categoryApi.update(category);
-    dispatch({
-      type: actions.CATEGORY_UPDATE,
-      payload: updatedCategory
-    });
+  return {
+    type: actions.CATEGORY_UPDATE,
+    payload: categoryApi.update(category)
   };
 }
 
 export function removeCategory(_id) {
-
-  return async dispatch => {
-    const category = await categoryApi.remove(_id);
-    if(category.removed) {
-      dispatch({
-        type: actions.CATEGORY_REMOVE,
-        payload: { _id }
-      });
-    }
-  };
-}
-
-export function loadingResponse(options) {
-  return async dispatch => {
-    dispatch({
-      type: actions.LOADING
-    });
-    try {
-      const response = await categoryApi.get(options);
-      dispatch({
-        type: actions.LOADED,
-        payload: response
-      });
-    }
-    catch(err) {
-      dispatch({
-        type: actions.ERROR,
-        payload: err
-      });
-    }
+  return {
+    type: actions.CATEGORY_REMOVE,
+    payload: { _id }
   };
 }
