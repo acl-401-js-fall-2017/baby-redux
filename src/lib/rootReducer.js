6723690bff5240ff34249ad3de63d20eca1e5474
Zac-L <Zachlichens@gmail.com>
2017-12-04 12:17:04 -0800

    removed response from load component.

--- src/lib/rootReducer.js
+++ src/lib/rootReducer.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
 import  categories  from '../category/reducer';
-import { response, loading, error } from '../load/reducer';
+import { loading, error } from '../app/reducer';
 
 const rootReducer =  combineReducers({ 
-  categories, response, loading, error
+  categories, loading, error
 });
 
 export default rootReducer;
