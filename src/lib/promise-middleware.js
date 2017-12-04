8305dbcc94ac1105b379c812b86cc649a4f49b4a
Zac-L <Zachlichens@gmail.com>
2017-12-04 12:16:39 -0800

    updated middleware to work properly

--- src/lib/promise-middleware.js
+++ src/lib/promise-middleware.js
@@ -1,32 +1,32 @@
 import * as actions from '../category/constants';
+
 const isPromise = val => val && typeof val.then === 'function';
 
+export default ({ dispatch }) => next => async action => {
+  
+  if(!isPromise(action.payload)) return next(action);
 
-export default function promiseMiddleware() {
-  return ({ dispatch }) => next => async action => {
 
-    if(!isPromise(action)) return next(action);
-
-    const { type, payload } = action;
+  const { type, payload } = action;
+  
+  dispatch({
+    type: actions.LOADING
+  });
+  
+  try {
+    const result = await payload;
+    dispatch({ type: actions.LOADED });    
+    dispatch({ 
+      type, 
+      payload: result
+    });
+  }
+  catch(err) {
     dispatch({
-      type: actions.LOADING
+      type: actions.ERROR,
+      payload: err
     });
 
-    try {
-      const result = await payload;
-      dispatch({ type: actions.LOADED });
-      dispatch({
-        type,
-        payload: result
-      });
-    }
-    catch(err) {
-      dispatch({
-        type: actions.ERROR,
-        payload: err
-      });
-
-      throw err;
-    }
-  };
-}
+    throw err;
+  }
+};
