74d9a36d638df4a2b48f0cab8b0ce65ab937fe01
ckrountree <jmail0207@gmail.com>
2017-12-04 10:04:04 -0800

    remove shortid

--- src/category/actions.js
+++ src/category/actions.js
@@ -1,6 +1,5 @@
 import * as actions from './constants';
 import categoriesApi from '../services/categories.api';
-// import shortid from 'shortid';
 
 export function loadCategory() {
   return async dispatch => {
