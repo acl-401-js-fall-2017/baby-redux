1787b1ee5a8c0a4c1dd1d9192926f647512c4e3a
ckrountree <jmail0207@gmail.com>
2017-12-04 11:50:47 -0800

    change order of reducer response

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -3,10 +3,10 @@ import { categoryReducer } from '../category/reducer';
 import { loadResponse, loadPage, loadError } from '../load/reducer';
 
 const rootReducer = combineReducers({
-  categoryReducer,
   loadResponse,
   loadPage,
   loadError,
+  categoryReducer,
 });
 
 export default rootReducer;
