78536159fc1a75ca6c76425f862fa15e3dc22925
Robin Miller <rmillerpdx@gmail.com>
2017-12-04 09:58:05 -0800

    create expense actions

---
+++ src/expenses/actions.js
@@ -0,0 +1,58 @@
+import { EXPENSE_LOAD, EXPENSE_ADD, EXPENSE_UPDATE, EXPENSE_REMOVE, LOADING, DONE_LOADING, ERROR } from './constants';
+import expenseApi from '../services/expenses-api';
+
+export function loadExpenses() {
+  return async dispatch => {
+    dispatch({ type: LOADING });
+
+    try {
+      const categories = await expenseApi.get();
+      dispatch({ type: DONE_LOADING });
+      dispatch({ type: EXPENSE_LOAD, payload: categories });
+    }
+
+    catch(err) {
+      dispatch({ type: DONE_LOADING });      
+      dispatch({ type: ERROR, payload: err });
+      throw err;
+    }
+
+  };
+}
+
+export function addCategory(expense) {
+  return async dispatch => {
+    dispatch({ type: LOADING });
+    const saved = await expenseApi.add(expense);
+    dispatch({ type: DONE_LOADING });
+    dispatch({ 
+      type: EXPENSE_ADD, 
+      payload: saved 
+    });
+  };
+}
+
+export function updateCategory(expense) {
+  return async dispatch => {
+    dispatch({ type: LOADING });    
+    const updated = await expenseApi.update(expense);
+    dispatch({ type: DONE_LOADING });
+    dispatch({
+      type: EXPENSE_UPDATE,
+      payload: updated
+    });
+  };
+}
+
+export function removeCategory(id) {
+  return async dispatch => {
+    dispatch({ type: LOADING });
+    await expenseApi.remove(id);
+    dispatch({ type: DONE_LOADING });
+    dispatch({
+      type: EXPENSE_REMOVE,
+      payload: id
+    });
+  };
+
+}
