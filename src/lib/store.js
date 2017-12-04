6086881eeae34fc489c7b5f866c84d0860615641
Zac-L <Zachlichens@gmail.com>
2017-12-04 10:41:54 -0800

    promisemiddleware not working yet

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,12 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+// import promiseMiddleware from './promise-middleware';
 import rootReducer from './rootReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(
+    applyMiddleware(
+      thunk,
+      // promiseMiddleware
+    )
+  )
 );
-
 export default store;
