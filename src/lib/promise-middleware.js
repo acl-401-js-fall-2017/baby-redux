f875d0dd873affecbcd3aa8962100923765d329c
Zac-L <Zachlichens@gmail.com>
2017-12-04 10:39:51 -0800

    added promise middle ware helper file

---
+++ src/lib/promise-middleware.js
@@ -0,0 +1,32 @@
+import * as actions from '../category/constants';
+const isPromise = val => val && typeof val.then === 'function';
+
+
+export default function promiseMiddleware() {
+  return ({ dispatch }) => next => async action => {
+
+    if(!isPromise(action)) return next(action);
+
+    const { type, payload } = action;
+    dispatch({
+      type: actions.LOADING
+    });
+
+    try {
+      const result = await payload;
+      dispatch({ type: actions.RESPONSE_LOAD });
+      dispatch({
+        type,
+        payload: result
+      });
+    }
+    catch(err) {
+      dispatch({
+        type: actions.ERROR,
+        payload: err
+      });
+
+      throw err;
+    }
+  };
+}
