ebe6ff67e7433ac7d6a51c93f041cac036097944
Robin Miller <rmillerpdx@gmail.com>
2017-12-05 10:45:34 -0800

    remove unnecessary expense state from store

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { combineReducers, applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { category, loading, error } from './category/reducer';
-import { expense } from './expenses/reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   category,
-  expense,
   loading,
   error
 });
