ebec8d936c82a063120b6be5389f5685cf182ead
ckrountree <jmail0207@gmail.com>
2017-12-04 17:21:54 -0800

    remove console.log

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Response from './load/Response';
 import Categories from './category/Categories';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const { loadPage, loadError } = this.props;
     return (
       <Router>
         <div className="App">
@@ -14,10 +17,25 @@ class App extends Component {
             <h1 className="App-title">Welcome to My Budget App</h1>
           </header>
           <Categories/>
+          <Response/>
+          {loadPage && 
+            <div className="loader">Patience...</div> }
+          {loadError && 
+              <div className="error">
+                {Array.isArray(loadError)
+                  ? <ul>loadError.map(err => <li>err</li></ul>
+                  : loadError.error ? loadError.error : loadError }
+              </div> }
         </div>
       </Router>
     );
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    loadPage: state.loadPage,
+    loadError: state.loadError
+  }),
+  null
+)(App);
