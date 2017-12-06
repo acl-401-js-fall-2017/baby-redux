d216a30e15ca89fed1101c0f438e4d4747f14ba8
Robin Miller <rmillerpdx@gmail.com>
2017-12-06 12:47:11 -0800

    shorten urls in bulma css

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import './styles/mystyles.css';
+import './styles/bulma.css';
 
 ReactDOM.render(
   <Provider store={store}>
