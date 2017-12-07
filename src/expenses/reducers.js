b744ac81d82554bc8fed8da84321714b5ae8a435
Shane Moyo <smoyo@pdx.edu>
2017-12-07 07:15:01 -0800

    all working

--- src/expenses/reducers.js
+++ src/expenses/reducers.js
@@ -6,17 +6,16 @@ export function expenses(state = {}, { type, payload }) {
     switch (type) {
     case actions.EXPENSE_ADD:
     state[payload.categoryId] = state[payload.categoryId] ? state[payload.categoryId] : [];
-    console.log('state', state);
-    console.log('payload', payload);
-    console.log('payload.categoryId', payload.categoryId)
-    console.log('state[payload.categoryId]',state[payload.categoryId])
     state[payload.categoryId].push(payload.expense);
       return { ...state };
     case actions.EXPENSE_REMOVE:
     state[payload.categoryId] = state[payload.categoryId].filter(expense => expense._id !== payload.expenseId);
       return { ...state };
     case actions.EXPENSE_UPDATE:
-    state[payload.categoryId] = state[payload.categoryId].map(expense => expense._id === payload.expense._Id ? { ...expense, ...payload.expense } : expense);
+    const newArray = state[payload.categoryId].map(expense => {
+      return expense._id === payload.expense._id ? { ...payload.expense } : expense
+    });
+    state[payload.categoryId] =  newArray;
       return { ...state };
     case actions.EXPENSE_LOAD:
     state[payload.categoryId] = payload.expenses;
