c956f9acab24636d0d295897a5c56e9ae2352d2d
Shane Moyo <smoyo@pdx.edu>
2017-12-07 08:15:33 -0800

    fixed indents on load comoponent

--- src/categories/Load.js
+++ src/categories/Load.js
@@ -1,20 +1,20 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 
+
 class Load extends PureComponent {
+
   render() {
     return (
-        <div>
-        <div className="loader">
-        </div>
-      {this.props.error && 
+      <div>
+        <div className="loader"></div>
+        {this.props.error && 
         <div className="error">
           {Array.isArray(this.props.error) 
             ? <ul>error.map(err => <li>err</li>)</ul>
             : this.props.error.error ? this.props.error.error : this.props.error
           }
-        </div>
-      }
+        </div>}
       </div> 
     );
   }
