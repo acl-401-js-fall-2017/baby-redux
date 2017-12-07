585249927f6b3b3ce8db179e4505db049abba719
Zac-L <Zachlichens@gmail.com>
2017-12-06 23:46:21 -0800

    added some conditionals on when to display certain forms and text

--- src/category/Categories.js
+++ src/category/Categories.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { StyledButton } from '../styles/styled';
 import { addCategory, updateCategory, removeCategory, loadCategories } from './actions';
+import styled from 'styled-components';
 
 import CategoryForm from './CategoryForm';
 import Expenses from '../expense/Expenses';
@@ -14,35 +15,34 @@ class Categories extends PureComponent {
 
   render() {
     const { categories, updateCategory, addCategory, removeCategory } = this.props;
-    
+
     return (
+
       <div>
-        <div>
-          <h4>Add a Budget</h4>
-          <CategoryForm onComplete={addCategory}></CategoryForm>
-          {categories.map(category => (
-            <div key={category._id}>
-              <h4>
-                {category.name} with budget of: ${category.budget}
-                {!category.showExpense && 
+        <h4>Add a Budget</h4>
+        <CategoryForm onComplete={addCategory}></CategoryForm>
+        {categories.map(category => (
+          <div key={category._id}>
+            <h4>
+              {category.name} with budget of: ${category.budget}
+            </h4>
+            {!category.showExpense && 
                   <StyledButton className="expensebtn" onClick={() => updateCategory({ ...category, showExpense: true })}>
                     Expenses
                   </StyledButton> 
-                }
-                {category.showExpense && 
+            }
+            {category.showExpense && 
                   <Expenses category={category}/>
-                }
-                {!category.showExpense &&
+            }
+            {!category.showExpense &&
                 <StyledButton className="removebtn" onClick={() => removeCategory(category._id)}>Remove</StyledButton>
-                }
-              </h4>
-              <h5> Update {category.name}'s Budget </h5>
-              <CategoryForm category={category} text="Update" 
-                onComplete={updateCategory}/>    
-            </div>
-          ))}
-        </div>
+            }
+            {!category.showExpense && <h5> Update {category.name}'s Budget </h5>}
+            {!category.showExpense && <CategoryForm category={category} text="Update" onComplete={updateCategory}/>}
+          </div>
+        ))}
       </div>
+
     );
   }
 }
