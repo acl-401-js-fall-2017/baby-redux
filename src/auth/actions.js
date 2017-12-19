2b3f21c471775d90d0bc00ddee1dcf7e369ea133
erdem koral <koral.erdem@gmail.com>
2017-12-19 11:10:15 -0800

    add auth actions and reducer

--- src/auth/actions.js
+++ src/auth/actions.js
@@ -1,3 +1,22 @@
-import { CHECKED_TOKEN, GOT_TOKEN, FETCHED_USER, LOGOUT, AUTH_FAILED  } from './constants';
-import AuthApi from '../services/authApi';
+import * as actions from './constants';
+import authApi from '../services/authApi';
 import { getStoredToken } from '../services/request';
+
+export function checkForToken() {
+  return dispatch => {
+    const token = getStoredToken();
+    if(!token) {
+      dispatch({ type: actions.CHECKED_TOKEN });
+      return;
+    }
+    dispatch({ type: actions.GOT_TOKEN, payload: token });
+    return authApi.verify()
+      .then(() => authApi.getUser())
+      .then(user => {
+        dispatch({ type: actions.FETCHED_USER, payload: user });
+      })
+      .catch(error => {
+        dispatch({ type: actions.AUTH_FAILED, payload: error });
+      });
+  };
+}
